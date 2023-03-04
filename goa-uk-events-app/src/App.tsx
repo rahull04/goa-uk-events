8d64f95b3d83d167d36ab0adc01fd0ce7d72745d
Rahul Bandodkar <rahul.bandodkar@creativecapsule.com>
2023-03-04 13:36:32 +0530

    Code cleanup

--- goa-uk-events-app/src/App.tsx
+++ goa-uk-events-app/src/App.tsx
@@ -3,12 +3,12 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './navigation/router';
 import Loader from './components/Loader';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <RouterProvider router={router} fallbackElement={<Loader />} />
     </div>
   );
-}
+};
 
 export default App;
